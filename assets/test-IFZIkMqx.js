62e6ef7d214c49b833513fa649aab790bfa4f29e
Igor Trujnara <itrujnara@wp.pl>
2025-03-09 17:39:57 +0100

    Updates

---
+++ assets/test-IFZIkMqx.js
@@ -0,0 +1,9 @@
+const t=`---\r
+title: Test\r
+lead: This is a test\r
+date: 2025-03-09\r
+tags: test\r
+---\r
+\r
+This is a test post.\r
+`;export{t as default};
---
+++ assets/test3-CGcPxCSS.js
@@ -0,0 +1,9 @@
+const t=`---\r
+title: Test 3\r
+lead: This is a third test\r
+date: 2025-03-05\r
+tags: sus\r
+---\r
+\r
+This is a test post.\r
+`;export{t as default};
